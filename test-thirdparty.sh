e421adfff63a5bc3828f7e522908f49d8858b9d9
reshi <reshlet.reshi@gmail.com>
2026-06-06 15:11:49 -0700

    Add more third-party test targets

--- test-thirdparty.sh
+++ test-thirdparty.sh
@@ -13,13 +13,17 @@ else
 fi
 
 tinycc_test_jobs=${TINYCC_TEST_JOBS:-1}
+libpng_test_jobs=${LIBPNG_TEST_JOBS:-$jobs}
+sqlite_test_jobs=${SQLITE_TEST_JOBS:-$jobs}
 
 usage() {
   cat <<EOF
-Usage: $0 [all|tinycc]
+Usage: $0 [all|tinycc|libpng|sqlite]
 
 Supported third-party tests:
   tinycc   Build TinyCC with ./chibicc and run TinyCC's test suite
+  libpng   Build libpng with ./chibicc and run libpng's test suite
+  sqlite   Build SQLite with ./chibicc and run SQLite's test suite
   all      Run every supported third-party test
 
 Environment:
@@ -28,6 +32,12 @@ Environment:
   TINYCC_COMMIT=SHA  TinyCC commit to test
   TINYCC_TEST_CC=CC  Host compiler command used by TinyCC's test harness
   TINYCC_TEST_JOBS=N Parallel jobs for TinyCC's test phase (default: 1)
+  LIBPNG_REPO=URL    libpng repository to clone
+  LIBPNG_COMMIT=SHA  libpng commit to test
+  LIBPNG_TEST_JOBS=N Parallel jobs for libpng's test phase (default: JOBS)
+  SQLITE_REPO=URL    SQLite repository to clone
+  SQLITE_COMMIT=SHA  SQLite commit to test
+  SQLITE_TEST_JOBS=N Parallel jobs for SQLite's test phase (default: JOBS)
 EOF
 }
 
@@ -83,8 +93,48 @@ run_tinycc() {
   )
 }
 
+run_libpng() {
+  local repo=${LIBPNG_REPO:-git@github.com:rui314/libpng.git}
+  local commit=${LIBPNG_COMMIT:-dbe3e0c43e549a1602286144d94b0666549b18e6}
+  local dir=$thirdparty_dir/libpng
+
+  cd "$root"
+  make chibicc
+  ensure_checkout libpng "$repo" "$commit"
+
+  (
+    cd "$dir"
+    CC="$root/chibicc" ./configure
+    sed -i 's/^wl=.*/wl=-Wl,/; s/^pic_flag=.*/pic_flag=-fPIC/' libtool
+    make -j"$jobs" clean
+    make -j"$jobs"
+    make -j"$libpng_test_jobs" test
+  )
+}
+
+run_sqlite() {
+  local repo=${SQLITE_REPO:-git@github.com:sqlite/sqlite.git}
+  local commit=${SQLITE_COMMIT:-86f477edaa17767b39c7bae5b67cac8580f7a8c1}
+  local dir=$thirdparty_dir/sqlite
+
+  cd "$root"
+  make chibicc
+  ensure_checkout sqlite "$repo" "$commit"
+
+  (
+    cd "$dir"
+    CC="$root/chibicc" CFLAGS=-D_GNU_SOURCE ./configure
+    sed -i 's/^wl=.*/wl=-Wl,/; s/^pic_flag=.*/pic_flag=-fPIC/' libtool
+    make -j"$jobs" clean
+    make -j"$jobs"
+    make -j"$sqlite_test_jobs" test
+  )
+}
+
 run_all() {
   run_tinycc
+  run_libpng
+  run_sqlite
 }
 
 if [ $# -eq 0 ]; then
@@ -98,6 +148,12 @@ case $1 in
     ;;
   tinycc)
     run_tinycc
+    ;;
+  libpng)
+    run_libpng
+    ;;
+  sqlite)
+    run_sqlite
     ;;
   -h|--help|help)
     usage
