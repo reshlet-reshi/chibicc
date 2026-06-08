944ca5e0f71e20bcec300696d05f86f67597e889
reshi <reshlet.reshi@gmail.com>
2026-06-07 19:53:19 -0700

    Give stage0 REPL a controlling tty

--- meta/stage0-init.sh
+++ meta/stage0-init.sh
@@ -59,7 +59,7 @@ fi
 
 if [ "$STAGE0_REPL" = 1 ]; then
   echo "stage0-qemu: entering BusyBox ash"
-  "$BB" ash
+  "$BB" setsid "$BB" cttyhack "$BB" ash
 fi
 
 "$BB" poweroff -f >/dev/null 2>&1 || "$BB" poweroff >/dev/null 2>&1 || true
